c690c84feb655b55e93ec1b1f291889ee3be6b47
Ravindu Hettiarachchi <ravindurrl147@gmail.com>
2025-06-17 12:43:17 +0530

    Add files via upload

---
+++ booking/src/main/java/com/example/booking/entity/Booking.java
@@ -0,0 +1,64 @@
+package com.example.booking.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "bookings")
+
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "no_of_days")
+    private int noOfDays;
+
+    @Column(name = "total_price")
+    private double totalPrice;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "starting_date")
+    private String startDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNoOfDays() {
+        return noOfDays;
+    }
+
+    public void setNoOfDays(int noOfDays) {
+        this.noOfDays = noOfDays;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+}
